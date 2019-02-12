c19e4ec765682a33fe6b52594d319b73beadcd1d
RGoldman <41007661+RaeGoldman@users.noreply.github.com>
2019-02-12 13:02:49 -0600

    Tournament Ready
    Change line 382 in prisoners_dilemma.py to add your team and any teams/examples you want to go up against.
    My code is in team14

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -35,19 +35,19 @@ import os.path
 from importlib import reload            
     
 import example0, example1, example2, example3
-#import example4, example5, example6, example7
+import example4, example5, example6, example7, example8
 import team0, team1, team2, team3, team4
 import team5, team6, team7, team8, team9
 import team10, team11, team12, team13, team14
 betray = example1
 collude = example0
 
-modules = [example0, example1, example2, example3, 
-team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, example4, example5, example6, example7,team10,
+modules = [example0, example1, example2, example3, example4, example5, example6, example7, example8,
+team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10,
 team11, team12, team13, team14]
 for module in modules:
     reload(module)
-    print ('reloaded',module)
+#    print ('reloaded',module)
     for required_variable in ['team_name', 'strategy_name', 'strategy_description']:
         if not hasattr(module, required_variable):
             setattr(module, required_variable, 'missing assignment')
@@ -241,13 +241,13 @@ def make_section1(modules, scores):
     for index in range(len(modules)):
         section1 += 'vs. P' + str(index) + ' :'
         for i in range(len(modules)):
-            section1 += '{:>7}'.format(round(scores[i][index]))
+            section1 += '{:>7}'.format(int(scores[i][index]))
         section1 += '\n'
 
     # Last line
     section1 += 'TOTAL  :'
     for index in range(len(modules)):
-        section1 += '{:>7}'.format(round(sum(scores[index]))     )
+        section1 += '{:>7}'.format(int(sum(scores[index]))     )
     return section1+'\n'
     
 def make_section2(modules, scores):
@@ -269,7 +269,7 @@ def make_section2(modules, scores):
     for index in range(len(modules)):
         section2_list.append((modules[index].team_name,
                               'P'+str(index),
-                              str(round(sum(scores[index]))/len(modules)),
+                              str(int((sum(scores[index]))/len(modules))),
                               str(modules[index].strategy_name)))
     section2_list.sort(key=lambda x: float(x[2]), reverse=True)
     
@@ -379,5 +379,5 @@ def post_to_file(string, filename='tournament.txt', directory=''):
  
 ### Call main_play() if this file is executed
 if __name__ == '__main__':
-    scores, moves, reports = main_play(modules[0:4])   
+    scores, moves, reports = main_play([team0, team3, team4, team6])
     section0, section1, section2, section3 = reports
