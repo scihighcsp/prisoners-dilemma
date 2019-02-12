215585dce843e4083764fc983c5bba5550effbb8
Jeysi Guillen <jguillen055.stu@noscihigh.org>
2019-02-12 06:16:12 -0800

    Team4
    Jeysi

--- team4.py
+++ team4.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Jeysi Guillen' # Only 10 chars displayed.
+strategy_name = 'Betraying'
+strategy_description = 'The first move will be betray.When ever they collude I collude. then when they betray I betray.' 
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,6 +17,16 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    if len(their_history)==0:
+       return 'b'
+    elif their_history[-1]=='c':
+        return 'c'
+    elif their_history[-1]=='b':
+        return 'b'
+    
+   
+   
+   
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,7 +36,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
