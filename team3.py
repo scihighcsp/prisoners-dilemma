4385ffbeac3547f99feb4274a8ed10e7969d2632
KylerTheVisionary <31045775+KylerTheVisionary@users.noreply.github.com>
2019-02-12 08:05:09 -0600

    Update team3.py

--- team3.py
+++ team3.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'FoxHound' # Only 10 chars displayed.
+strategy_name = 'The Best offense Is The Best Defense'
+strategy_description = 'Swap between collide and betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
